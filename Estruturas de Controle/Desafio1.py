d7be6ec88dbd16a83e7b0bcb3aea4cf542732332
Angelo Mutti <mutti_angelo23@hotmail.com>
2021-03-05 17:06:34 -0300

    importacao 2

---
+++ Estruturas de Controle/Desafio1.py
@@ -0,0 +1,37 @@
+def nota() = 10
+
+if nota <=10>=9.1:
+    print('nota A')
+
+elif nota<=9>=8.1:
+    print('Nota A-')
+
+elif nota <=8>=7.1:
+    print("Nota B")
+
+elif nota <=7>=6.1:
+    print('Nota B-')
+
+elif nota <=6>=5.1:
+    print("Nota C")
+
+elif nota <=5>=4.1:
+    print("Nota C-")
+
+elif nota <=4>= 3.1:
+    print("Nota D")
+
+elif nota <=3>= 2.1:
+    print("Nota D-")
+
+elif nota <=3>= 2.1:
+    print("Nota F")
+
+elif nota <=2>=1.1:
+    print("Nota F-")
+    
+elif nota <=1>=0:
+    print("Nota 0")
+
+else:
+    print("nota inválida")
